1d12aec97ea1642e45bafb2f6cd814b2d8df0d54
Battlesquid <25509915+Battlesquid@users.noreply.github.com>
2022-03-07 20:00:17 -0700

    add default permission

--- src/interactions/timeout.ts
+++ src/interactions/timeout.ts
@@ -3,6 +3,7 @@ import { SlashCommandBuilder } from "@discordjs/builders";
 const timeoutInteraction = new SlashCommandBuilder()
     .setName("timeout")
     .setDescription("Timeout a user")
+    .setDefaultPermission(false)
     .addUserOption(option =>
         option
             .setName("user")
