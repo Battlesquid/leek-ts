00be9a38b71f7e12b4ed952cc5ba4bf56477c36c
Battlesquid <25509915+Battlesquid@users.noreply.github.com>
2023-07-04 11:19:08 -0700

    incomplete: remove dead code

--- src/modals/index.ts
+++ src/modals/index.ts
@@ -1,10 +1,3 @@
-export enum ReactRoleModal {
-    TitleInput = "title_input",
-    DescInput = "desc_input",
-    ColorInput = "color_input",
-    MsgInput = "msg_input"
-}
-
 export enum VerifyRequestModal {
     Id = "verify_request",
     NameInput = "name_input",
